0fd7e80b194ca588decda32cde3b3d326cb16a96
Oliver Burnett-Hall <olly@burnett-hall.co.uk>
2024-06-26 17:18:00 +0100

    [18CH] Use game-class methods for Gotthard hex and minors

--- lib/engine/game/g_18_ch/graph.rb
+++ lib/engine/game/g_18_ch/graph.rb
@@ -8,21 +8,17 @@ module Engine
       class Graph < G1858::Graph
         private
 
-        GOTTHARD_HEX = 'H11'
-        PRIVATE_BROAD_GAUGE = 'GB'
-        PRIVATE_NARROW_GAUGE = 'FOB'
-
         def path_node(path, entity)
           node = G1858::Part::PathNode.new(path)
-          return node unless path.hex.coordinates == GOTTHARD_HEX
+          return node unless path.hex == @game.gotthard
 
           # The pre-printed Gotthard hex is a special case. It is a home hex
           # for two private railways (FOB and GB) and it has two paths, one
           # broad gauge and one metre (narrow) gauge. FOB is only allowed to
           # trace routes from this hex using the metre gauge path, and GB can
           # only use the broad gauge path.
-          return node if (path.track == :broad && entity.id == PRIVATE_BROAD_GAUGE) ||
-                         (path.track == :narrow && entity.id == PRIVATE_NARROW_GAUGE)
+          return node if (path.track == :broad && entity == @game.gb_minor) ||
+                         (path.track == :narrow && entity == @game.fob_minor)
         end
       end
     end
