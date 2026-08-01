7a244b0ff1c4ddcdf19a2dbbd57e5bc80ee3cb81
Oliver Burnett-Hall <olly@burnett-hall.co.uk>
2026-08-02 00:42:06 +0100

    [Route Graph] Add unit tests for maps with lots of junctions
    This adds tests for two 19-hex maps. These are a hexagonal shape, with
    one central hex and two rings of hexes around it. Both versions have two
    cities, at opposite ends of the map.
    
    One map has all the non-city hexes as plain track with Lawson tiles
    (6-way, 4-way or 3-way junction). The other has curvilinear track with
    3, 4 or 6 two-way converging junctions on each tile.
    
    The curvilinear/converging junction map is one where the Engine::Graph
    walk algorithm performs extremely poorly.

--- spec/lib/engine/route_graph/graph_walker_spec.rb
+++ spec/lib/engine/route_graph/graph_walker_spec.rb
@@ -381,6 +381,60 @@ module Engine
           expect(b6_path_edges).to match_array([[1, 5], [2, 5]])
         end
       end
+
+      describe 'on a highly connected map with Lawson tiles' do
+        let(:hexes) { { white: { %w[A3 A5 A7 B2 B4 B6 B8 C1 C3 C5 C7 C9 D2 D4 D6 D8 E3 E5 E7] => '' } } }
+        let(:tiles) { { '12' => 2, '60' => 7, '80' => 4, '545' => 6 } }
+
+        before :each do
+          lay_tile('C1', '12',  5, 0)
+          lay_tile('C9', '12',  2, 1)
+          lay_tile('A3', '80',  4, 0)
+          lay_tile('A7', '80',  3, 1)
+          lay_tile('E3', '80',  0, 2)
+          lay_tile('E7', '80',  1, 3)
+          %w[B4 B6 C3 C5 C7 D4 D6].each_with_index { |hex, i| lay_tile(hex, '60',  0, i) }
+          %w[E5 D8 B8 A5 B2 D2].each_with_index    { |hex, i| lay_tile(hex, '545', i, i) }
+          hex('C1').tile.cities.first.place_token(alpha, alpha.next_token, free: true)
+        end
+
+        it 'can reach all hexes' do
+          all_hexes = hexes.map { |_color, hexdefs| hexdefs.keys }.flatten
+          expect(walker.reachable_hexes.map(&:coordinates)).to match_array(all_hexes)
+        end
+
+        it 'can reach all paths' do
+          all_paths = game.hexes.map(&:tile).flat_map(&:paths)
+          expect(walker.connected_paths).to match_array(all_paths)
+        end
+      end
+
+      describe 'on a highly connected map with converging junctions' do
+        let(:hexes) { { white: { %w[A3 A5 A7 B2 B4 B6 B8 C1 C3 C5 C7 C9 D2 D4 D6 D8 E3 E5 E7] => '' } } }
+        let(:tiles) { { '12' => 2, '39' => 4, '43' => 6, '114' => 7 } }
+
+        before :each do
+          lay_tile('C1', '12',  5, 0)
+          lay_tile('C9', '12',  2, 1)
+          lay_tile('A3', '39',  4, 0)
+          lay_tile('A7', '39',  3, 1)
+          lay_tile('E3', '39',  0, 2)
+          lay_tile('E7', '39',  1, 3)
+          %w[B4 B6 C3 C5 C7 D4 D6].each_with_index { |hex, i| lay_tile(hex, '114', 0, i) }
+          %w[E5 D8 B8 A5 B2 D2].each_with_index    { |hex, i| lay_tile(hex, '43',  i, i) }
+          hex('C1').tile.cities.first.place_token(alpha, alpha.next_token, free: true)
+        end
+
+        it 'can reach all hexes' do
+          all_hexes = hexes.map { |_color, hexdefs| hexdefs.keys }.flatten
+          expect(walker.reachable_hexes.map(&:coordinates)).to match_array(all_hexes)
+        end
+
+        it 'can reach all paths' do
+          all_paths = game.hexes.map(&:tile).flat_map(&:paths)
+          expect(walker.connected_paths).to match_array(all_paths)
+        end
+      end
     end
   end
 end
